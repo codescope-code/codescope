7286401517ab58348d8bdc343cc6ff1c6e6b734f
codescope-code <codescope.tech@gmail.com>
2025-06-21 21:51:41 +0300

    Navbar.jsx

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar() {
       <nav className="flex justify-between items-center px-6 py-4   ">
         <div className="flex items-center gap-2">
           <Image src="/logo1.png" alt="Logo" width={70} height={70} />
-          <Link href="html" className="font-bold text-3xl">
+          <Link href="#home" className="font-bold text-3xl">
           CodeScope
           </Link>
         </div>
